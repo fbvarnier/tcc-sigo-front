76676e15a0861d05c47f727a9e5f843b7f97703c
Fabiano <fabiano.varnier@luizalabs.com>
2021-11-02 23:52:42 -0300

    another fix

--- src/app/hello.service.ts
+++ src/app/hello.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HelloService {
   
+
+  webapiurl = environment.webapiurl;
 
   constructor( private httpClient: HttpClient) { }
 
@@ -17,6 +20,6 @@ export class HelloService {
     .append('Access-Control-Allow-Methods','GET')
     .append('Access-Control-Max-Age','86400');    // cache for 1 day
 
-    return this.httpClient.get("http://35.198.51.5/", {headers: header, responseType: 'text'});
+    return this.httpClient.get(this.webapiurl, {headers: header, responseType: 'text'});
   }
 }
